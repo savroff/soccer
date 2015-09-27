1069e0af659e6fe56fd63c56588513f7783f50c5
Nick Savrov <savroff@MacBook-Pro-Nikita-2.local>
2015-09-27 12:38:21 -0400

    Change boot

--- boot.rb
+++ boot.rb
@@ -1,12 +1,12 @@
 require 'rubygems'
 require 'bundler'
+Bundler.setup
 
-# require 'active_support'
-require 'activerecord'
+require 'active_record'
 require 'active_support/core_ext'
 require 'active_support/dependencies'
 
-ActiveSupport::Dependencies.autoload_path += %w'
+ActiveSupport::Dependencies.autoload_paths += %w'
   app/models
   app/api
   app/services
